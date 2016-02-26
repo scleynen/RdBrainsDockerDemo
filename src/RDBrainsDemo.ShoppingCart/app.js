fe0aa699f5da3b97323b12512088a67057606916
scleynen <steve.cleynen@hotmail.com>
2016-02-26 22:45:16 +0100

    With redis

--- src/RDBrainsDemo.ShoppingCart/app.js
+++ src/RDBrainsDemo.ShoppingCart/app.js
@@ -1,33 +1,45 @@
 ﻿var express = require('express'),
-    bodyParser = require('body-parser'),
+    redis = require('redis'),
     cookieParser = require('cookie-parser'),
-    expressSession = require('express-session');
+    session = require('express-session'),
+    redisStore = require('connect-redis')(session),
+    bodyParser = require('body-parser');
 
-var app = express();
+var client = redis.createClient('6379','demoredis'),//CREATE REDIS CLIENT
+    app = express();
+
+client.on('connect', function () {
+    console.log('connected');
+});
+
+app.use(cookieParser('yoursecretcode'));
+app.use(session(
+    {
+        secret: 'yourothersecretcode', 
+        store: new redisStore({ host: 'demoredis', port: 6379, client: client }),
+        cookie: { expires: new Date(253402300000000)},
+        saveUninitialized: true, // don't create session until something stored,
+        resave: true // don't save session if unmodified
+    }
+));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser('mysecretcode'));
-app.use(expressSession({
-    secret: 'myothersecretcode', 
-    cookie: { maxAge: 7 * 24 * 60 * 3600 * 1000 },
-    saveUninitialized: true, 
-    resave: true
-}));
 
 var router = express.Router();
 
-function CartItem(product){
+function CartItem(product) {
     this.product = product;
 }
 
 router.post('/addcart/:product', function (req, res) {
     var cartItem = new CartItem(req.params.product);
     if (req.session.cart) {
-       req.session.cart.push(cartItem);
+        req.session.cart.push(cartItem);
     }
     else {
         req.session.cart = new Array(cartItem);
-     }
+    }
     
     res.send("product added to cart")
 });
@@ -43,5 +55,9 @@ router.get('/getcart', function (req, res) {
 
 app.use('/', router);
 var server = app.listen(5002, function () {
-    console.log('BASIC SESSION server is listening on port %d', server.address().port);
+    console.log('REDIS SESSION server is listening on port %d', server.address().port);
 });
+
+
+
+
